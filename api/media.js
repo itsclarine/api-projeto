a360a8a1a23dbe857f11398e1c817549d9477399
clara <125755948+itsclarine@users.noreply.github.com>
2026-05-08 07:48:14 -0300

    add media.js

---
+++ api/media.js
@@ -0,0 +1,37 @@
+const medias = [
+    {
+        id: 1,
+        type: "movie",
+        title: "Donnie Darko"
+    },
+    {
+        id: 2,
+        type: "serie",
+        title: "Breaking Bad"
+    },
+    {
+        id: 3,
+        type: "movie",
+        title: "Black Swan"
+    },
+    {
+        id:4,
+        type: "serie",
+        title: "Game of Thrones"
+    },
+]
+ 
+module.exports = (req, res) => {
+    if (req.method === "GET"){
+        return res.status(200).json({medias});
+    }else if (req.method === "POST"){
+        const newMedia = req.body;
+        return res.status(201).json({
+            message: "Media Created",
+            media: newMedia,
+            totalMedias: [...medias, newMedia].length,
+            allMedias:[...medias, newMedia]
+        }
+        );
+    }
+}
